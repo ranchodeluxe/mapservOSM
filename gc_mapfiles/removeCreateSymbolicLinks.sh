7d6d81a12dacc586f0cf233014f0fef3597136dd
gcorradini <gregcorradini@gmail.com>
2011-04-19 05:15:20 -0700

    removed gc_mapfiles from repository

--- gc_mapfiles/removeCreateSymbolicLinks.sh
+++
@@ -1,6 +0,0 @@
-sudo rm /usr/lib/cgi-bin/mapservOSM
-sudo rm /mnt/z-raid6/projects/cugos/osm/www_root/mapservOSM
-sudo ln -s /home/gcorradini/REPOS/GIT/mapservOSM /usr/lib/cgi-bin/mapservOSM
-sudo ln -s /home/gcorradini/REPOS/GIT/mapservOSM  /mnt/z-raid6/projects/cugos/osm/www_root/mapservOSM
-
-
